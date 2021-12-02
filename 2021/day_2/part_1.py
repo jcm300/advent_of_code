61d82b1ae81297d7f9706b1263fde371785e3748
jcm300 <zeclmartins@gmail.com>
2021-12-02 22:34:12 +0000

    Add Day 2 of 2021

---
+++ 2021/day_2/part_1.py
@@ -0,0 +1,34 @@
+import os
+import sys
+
+# Read arguments
+if len(sys.argv) != 2:
+    raise ValueError('Please provide a filename input')
+
+filename = sys.argv[1]
+
+# Read file
+file_data = open(os.getcwd() + '/' + filename, 'r')
+
+# Parse file
+commands = []
+for line in file_data.readlines():
+    commands.append(line.split(' '))
+
+# Get answer
+horizontal_pos = 0
+depth_pos = 0
+
+for command in commands:
+    direction = command[0]
+    value = int(command[1])
+
+    if direction == 'forward':
+        horizontal_pos += value
+    elif direction == 'down':
+        depth_pos += value
+    elif direction == 'up':
+        depth_pos -= value
+
+answer = horizontal_pos * depth_pos
+print(answer)
