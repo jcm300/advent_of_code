763909dba6b27e4eb60c707bb1d9b56b6126a084
jcm300 <zeclmartins@gmail.com>
2021-12-26 19:28:02 +0000

    Add Day 12 of 2021

---
+++ 2021/day_12/part_1.py
@@ -0,0 +1,66 @@
+import os
+import re
+import sys
+import math
+
+# Read arguments
+if len(sys.argv) != 2:
+    raise ValueError('Please provide a filename input')
+
+filename = sys.argv[1]
+
+# Read file
+file_data = open(os.getcwd() + '/' + filename, 'r')
+
+#
+# Parse file
+#
+vertices = set()
+lines = []
+
+for line in file_data.readlines():
+    l = line.replace('\n', '')
+    p1, p2 = l.split('-')
+
+    vertices.add(p1)
+    vertices.add(p2)
+    lines.append({'p1': p1, 'p2': p2})
+
+connections = {}
+
+for v in vertices:
+    connections[v] = set()
+
+for line in lines:
+    p1 = line['p1']
+    p2 = line['p2']
+
+    connections[p1].add(p2)
+    connections[p2].add(p1)
+
+#
+# Get answer
+#
+paths = []
+
+def try_path(connections, conn, path):
+    paths = []
+    path += ',' + conn
+    
+    if conn == 'end':
+        paths.append(path)
+
+    else:
+        for c in connections[conn]:
+            if c.upper() == c or c not in path:
+                ps = try_path(connections, c, path)
+                paths += ps
+
+    return paths
+
+for conn in connections['start']:
+    ps = try_path(connections, conn, 'start')
+    paths += ps
+
+answer = len(paths)
+print(answer)
